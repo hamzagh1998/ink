fd8c72ee6bae3c7729c6f0b32633deb5ad59f078
hamzagh1998 <hamzaghenimi98@gmail.com>
2024-04-10 01:14:17 +0100

    add collapse animation

--- app/(main)/_components/workspace.tsx
+++ app/(main)/_components/workspace.tsx
@@ -1,4 +1,3 @@
-"use client";
 import { useState } from "react";
 import { ChevronDown, ChevronRight } from "lucide-react";
 import { cn } from "@/lib/utils";
@@ -19,7 +18,7 @@ export function Workspace({
           paddingLeft: level ? `${level * 12 + 12}px` : "12px",
         }}
         onClick={() => setIsOpen(!isOpen)}
-        className="flex justify-between items-center gap-2 group min-h-[27px] text-sm py-1 pr-3 my-4 w-full hover:bg-primary/5 text-muted-foreground font-medium cursor-pointer transition duration-300 ease-in-out"
+        className="flex justify-between items-center gap-2 group min-h-[27px] text-sm py-1 pr-3 my-4 w-full hover:bg-primary/5 text-muted-foreground font-medium cursor-pointer"
       >
         <div className="flex justify-center items-center gap-2">
           {isOpen ? <ChevronDown size={18} /> : <ChevronRight size={18} />}
@@ -28,8 +27,9 @@ export function Workspace({
       </div>
       <div
         className={cn(
-          "ml-3 transition duration-300 ease-in-out",
-          isOpen ? "h-auto" : "h-0 overflow-hidden"
+          "ml-3 transition-all duration-200 ease-in-out",
+          !isOpen && "h-0 opacity-0",
+          isOpen && "opacity-100"
         )}
       >
         {children}
